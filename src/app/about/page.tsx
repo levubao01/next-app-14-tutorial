6b529a8441a8e6065ca0797468e95cd9c3361948
unknown <lebao211101@gmail.com>
2025-10-03 14:27:05 +0700

    Bài 17: Routing metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+export const metadata = {
+	title: 'About Page',
+	description: 'About Page',
+}
+
 const AboutPage = () => {
 	return <div>AboutPage</div>
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,6 +1,25 @@
+import { Metadata } from 'next'
 import React from 'react'
 
-const ProductDetail = ({ params }: { params: { productId: string } }) => {
+type ProductDetailProps = {
+	params: {
+		productId: string
+	}
+}
+
+export const generateMetadata = async ({ params }: ProductDetailProps): Promise<Metadata> => {
+	const title = await new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(`Product ${params.productId}`)
+		}, 1000)
+	})
+	return {
+		title: title as string,
+		description: `Product ${params.productId} description`,
+	}
+}
+
+const ProductDetail = ({ params }: ProductDetailProps) => {
 	return <div>ProductDetail {params.productId}</div>
 }
 
